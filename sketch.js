cb97a1f2d3363d2606b995da106190082f9c848f
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-24 09:40:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,11 @@
-var weight = [35,38,40,45];
 
-
-function setup() {
+function setup() 
+{
   createCanvas(400,400);
-
-  var sum = weight[0] + weight[1] + weight[2] + weight[3];
-  var average = sum/weight.length;
-  console.log(average);
-
 }
 
 function draw() 
 {
- 
 background(51);
 
 }
